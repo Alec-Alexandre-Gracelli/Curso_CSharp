9399e21e93edce7f5e196d176603baf5b45dd81c
Alec_Gracelli <alecgracelli@gmail.com>
2022-11-10 17:19:40 -0300

    Create Atividade3

---
+++ Curso_CSharp_Atividade3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+
+namespace Curso_CSharp_Atividade3
+{
+    class Program
+    {
+        static double Pi = 3.14;
+
+        static void Main(string[] args)
+        {
+            Console.Write("Entre o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Circunferencia(raio);
+            double volume = Volume(raio);
+
+            Console.WriteLine($"Circunferência: {circ.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Volume: {volume.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Valor de PI: {Pi}");
+        }
+
+        static double Circunferencia(double r)
+        {
+            return 2.0 * Pi * r;
+        }
+
+        static double Volume(double r)
+        {
+            return 4.0 / 3.0 * Pi * Math.Pow(r, 3);
+        }
+
+
+    }
+}
