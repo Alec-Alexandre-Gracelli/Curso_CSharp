994e0f5e53952fae68f217ec2ec07afc681c8779
Alec_Gracelli <alecgracelli@gmail.com>
2022-11-11 19:22:14 -0300

    Fix Atividade3

---
+++ Curso_CSharp_Atividade3/Calculadora.cs
@@ -0,0 +1,22 @@
+﻿namespace Curso_CSharp_Atividade3
+{
+    internal class Calculadora
+    {
+        //Versão 2 com membros de instância
+
+        //public double Pi = 3.14;
+        //public double Circunferencia(double r)
+        //public double Volume(double r)
+
+        //Versão 3 com método estático
+        public static double Pi = 3.14;
+        public static double Circunferencia(double r)
+        {
+            return 2.0 * Pi * r;
+        }
+        public static double Volume(double r)
+        {
+            return 4.0 / 3.0 * Pi * Math.Pow(r, 3);
+        }
+    }
+}
--- Curso_CSharp_Atividade3/Program.cs
+++ Curso_CSharp_Atividade3/Program.cs
@@ -2,36 +2,59 @@
 
 namespace Curso_CSharp_Atividade3
 {
+    //Versão 1
     // métodos na própria classe do programa
     // ...Nota: dentro de um método estático você não pode chamar membros de instância da mesma classe.
 
     class Program
     {
-        static double Pi = 3.14;
-
         static void Main(string[] args)
         {
+            //Versão 1
+            // métodos na própria classe do programa
+            // ...Nota: dentro de um método estático você não pode chamar membros de instância da mesma classe.
+
+            //static double Pi = 3.14;
+
+            //static void Main(string[] args)
+            //{
+            //    Console.Write("Entre o valor do raio: ");
+            //    double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //    double circ = Circunferencia(raio);
+            //    double volume = Volume(raio);
+
+            //    Console.WriteLine($"Circunferência: {circ.ToString("F2", CultureInfo.InvariantCulture)}");
+            //    Console.WriteLine($"Volume: {volume.ToString("F2", CultureInfo.InvariantCulture)}");
+            //    Console.WriteLine($"Valor de PI: {Pi}");
+            //}
+
+            //static double Circunferencia(double r)
+            //{
+            //    return 2.0 * Pi * r;
+            //}
+
+            //static double Volume(double r)
+            //{
+            //    return 4.0 / 3.0 * Pi * Math.Pow(r, 3);
+            //}
+
+
+            //Versão 2 com membros de instância
+            //Calculadora calc = new Calculadora();
+            //double circ = calc.Circunferencia(raio);
+            //double volume = Calculadora.Volume(raio);
+
+            //Versão 3 com método estático
             Console.Write("Entre o valor do raio: ");
             double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double circ = Circunferencia(raio);
-            double volume = Volume(raio);
+            double circ = Calculadora.Circunferencia(raio);
+            double volume = Calculadora.Volume(raio);
 
             Console.WriteLine($"Circunferência: {circ.ToString("F2", CultureInfo.InvariantCulture)}");
             Console.WriteLine($"Volume: {volume.ToString("F2", CultureInfo.InvariantCulture)}");
-            Console.WriteLine($"Valor de PI: {Pi}");
+            Console.WriteLine($"Valor de PI: {Calculadora.Pi}");
         }
-
-        static double Circunferencia(double r)
-        {
-            return 2.0 * Pi * r;
-        }
-
-        static double Volume(double r)
-        {
-            return 4.0 / 3.0 * Pi * Math.Pow(r, 3);
-        }
-
-
     }
 }
