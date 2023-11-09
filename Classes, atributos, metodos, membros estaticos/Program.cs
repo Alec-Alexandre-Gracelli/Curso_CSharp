78114a5d7c1555f4c8cf40b761c9817e482cf9e0
Alec_Gracelli <alecgracelli@gmail.com>
2023-11-09 16:10:19 -0300

    finaliznado aula

--- Classes, atributos, metodos, membros estaticos/Program.cs
+++ Classes, atributos, metodos, membros estaticos/Program.cs
@@ -26,7 +26,6 @@ namespace Course
             }
         }
 
-        static double Pi = 3.14;
         static void AreaTriangulo()
         {
             Triangulo x, y;
@@ -95,26 +94,19 @@ namespace Course
 
         static void Raio()
         {
+
             Console.WriteLine("Entre o valor do raio: ");
             double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double circ = Circunferencia(raio);
-            double volume = Volume(raio);
+            double circ = Calculadora.Circunferencia(raio);
+            double volume = Calculadora.Volume(raio);
 
             Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
             Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine("Valor de PI: " + Pi.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
         }
 
-        static double Circunferencia(double r)
-        {
-            return 2.0 * Pi * r;
-        }
-
-        static double Volume(double r)
-        {
-            return 4.0 / 3.0 * Pi * r * r * r;
-        }
+        
     }
 
 }
