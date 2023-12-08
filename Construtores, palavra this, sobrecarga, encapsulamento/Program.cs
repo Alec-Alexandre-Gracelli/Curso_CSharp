0eadfd8f6e6de1e4a6bd3dd256e5604f0589fa9b
Alec_Gracelli <alecgracelli@gmail.com>
2023-12-08 16:26:30 -0300

    aula encapsulamento

--- Construtores, palavra this, sobrecarga, encapsulamento/Program.cs
+++ Construtores, palavra this, sobrecarga, encapsulamento/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Construtores__palavra_this__sobrecarga__encapsulamento;
 using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
@@ -17,45 +18,15 @@ class Program
                 Console.WriteLine("Escolha inválida.");
                 break;
         }
+    }
 
-        static void Produto()
-        {
-            Console.WriteLine("Entre os dados do produto:");
-            Console.Write("Nome: ");
-            string nome = Console.ReadLine();
-            Console.Write("Preço: ");
-            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-           
-            Produto produto = new Produto(nome, preco);
+    static string Produto()
+    {
+        Produto produto = new Produto("TV", 500.00, 10);
 
-            Produto produto1 = new Produto();
+        produto.SetNome("TV 4k"); 
 
-            Produto produto2 = new Produto 
-            { 
-                Nome = "TV", 
-                Preco = 500.00, 
-                Quantidade = 20 
-            };
-
-            Console.WriteLine();
-            Console.WriteLine("Dados do produto: " + produto);
-
-            Console.WriteLine();
-            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
-            int qte = int.Parse(Console.ReadLine());
-            produto.AdicionarProdutos(qte);
-
-            Console.WriteLine();
-            Console.WriteLine("Dados atualizados: " + produto);
-
-            Console.WriteLine();
-            Console.Write("Digite o número de produtos a ser removido do estoque: ");
-            qte = int.Parse(Console.ReadLine());
-            produto.RemoverProdutos(qte);
-
-            Console.WriteLine();
-            Console.WriteLine("Dados atualizados: " + produto);
-
-        }
+        Console.WriteLine(produto.GetNome());
+        return produto.GetNome();
     }
 }
